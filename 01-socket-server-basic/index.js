1069cca240e013fd5d2595790e6b4adb5606de77
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-10-02 19:25:47 -0500

    add basic chat app

---
+++ 01-socket-server-basic/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  socket.on("message-to-server", (data) => {
+    console.log(data);
+    // io emits to all connected clients
+    io.emit("message-to-client", data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
